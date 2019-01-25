0b56108e7b841528af9d8990576c6b128e3b3c11
robertobuso <robertobuso@gmail.com>
2019-01-25 15:22:21 -0500

    working with input box styling email destop 1

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 import CallToAction from './Components/CallToAction.js'
+import SignUpForm from './Components/SignUpForm.js'
 
 class App extends Component {
   render() {
     return (
       <div className='footer'>
-      <div className='rectangle'>
-      <CallToAction />
-      </div>
+        <div className='rectangle'>
+
+            <CallToAction />
+            <SignUpForm />
+
+        </div>
       </div>
     );
   }
---
+++ src/Components/SignUpForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SignUpForm = props => {
+
+  return (
+    <>
+    <form>
+    <header className='form-header'>
+    SIGN UP FOR THE TLC NEWSLETTER.
+    </header>
+    <input className='email-input email-text'
+    name='email' />
+  </form>
+</>
+  )
+}
+
+export default SignUpForm;
