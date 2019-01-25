8ad306bd7afe7f1a1d68f64f0d87b83ccbf662ba
robertobuso <robertobuso@gmail.com>
2019-01-25 14:53:45 -0500

    footer rectangle call to action in desktop 1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import CallToAction from './Components/CallToAction.js'
+
 class App extends Component {
   render() {
     return (
-      <div>
-      Hello Worldo
+      <div className='footer'>
+      <div className='rectangle'>
+      <CallToAction />
+      </div>
       </div>
     );
   }
---
+++ src/Components/CallToAction.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CallToAction = props => {
+  return (
+    <div className='call-to-action'>
+    Join {<br/>}the list
+    </div>
+  )
+}
+
+export default CallToAction;
